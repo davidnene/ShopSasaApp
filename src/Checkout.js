af5996148c5801f6d076351263dd805f9fbbcc33
davidnene <davienesh4@gmail.com>
2022-07-31 17:33:11 +0300

    Update code

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,9 +4,13 @@ import CheckoutCard from "./CheckoutCard"
 function Checkout({ addToCart, handleToCart, setHandleToCart }) {
 return (
     <div>
-        {addToCart.map(item =>        
-            (item.toCart? <CheckoutCard addToCart={addToCart} title = {item.title} price = {item.price} image = {item.image} key={item.id} id={item.id} handleToCart={handleToCart} setHandleToCart={setHandleToCart}/>: null
-        ))}
+        <div className="container">
+            <div className="row">
+                {addToCart.map(item =>        
+                    (item.toCart? <CheckoutCard addToCart={addToCart} title = {item.title} price = {item.price} image = {item.image} key={item.id} id={item.id} handleToCart={handleToCart} setHandleToCart={setHandleToCart}/>: null
+                ))}
+            </div>
+        </div>
     </div>
 )
 };
