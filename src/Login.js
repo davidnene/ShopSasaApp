decb34ed4733117ae8a2bf8a3383125475229878
davidnene <davienesh4@gmail.com>
2022-07-31 16:50:51 +0300

    Add placeholder attr

--- src/Login.js
+++ src/Login.js
@@ -20,12 +20,12 @@ function Login() {
             <form onSubmit={handleLogin}>
                 <div className="mb-3">
                   <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                  <input required type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                  <input required type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="try: name@shopsasa.africa"/>
                   {/* <div id="emailHelp" className="form-label">We'll never share your email with anyone else.</div> */}
                 </div>
                 <div className="mb-3">
                   <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                  <input required type="password" className="form-control" id="exampleInputPassword1"/>
+                  <input required type="password" className="form-control" id="exampleInputPassword1" placeholder="try: @123"/>
                 </div>
                 <button style={{position:"absolute", left:"100px"}} type="submit" className="btn btn-warning">Login</button>
             </form>
