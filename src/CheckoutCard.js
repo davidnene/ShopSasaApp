24e662490c213325d19f802c83295fca2dc48f68
davidnene <davienesh4@gmail.com>
2022-07-31 14:50:33 +0300

    Add CheckoutCard.js

---
+++ src/CheckoutCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function CheckoutCard({ addToCart, title, price, image, id, handleToCart, setHandleToCart}) {
+    console.log(addToCart)
+    function handleAddClick() {
+        setHandleToCart(!handleToCart)
+    }
+
+    const btn = handleToCart?"btn btn-success":"btn btn-warning"
+    return (
+        <div className='col-3'>
+        <div className="card gx-1" style={{width: 18 + "rem", height: 28 + "rem"}}>
+            <img style={{height: 200 + "px",width:200 + "px"}} src={image} className="card-img-top" alt={title}/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">${price}</p>
+                <button className={btn} onClick={handleAddClick}>{handleToCart?"Remove from Cart":"Add to Cart"}</button><br/>
+                <Link exact="true" to={`/products/${id}`} >See details</Link>     
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default CheckoutCard;
